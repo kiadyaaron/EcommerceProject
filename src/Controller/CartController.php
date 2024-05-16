19b2e0f439714fd23baa0c6ccb133d8caa4314a7
kiadyaaron <aaronkiady@gmail.com>
2024-05-16 17:05:13 +0200

    kaomity

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,24 +3,53 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class CartController extends AbstractController
 {
-    #[Route('/mon-panier', name:'cart')]
-    public function index(): Response
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('cart/index.html.twig');
+        $this->entityManager = $entityManager;
     }
 
-    #[Route('/cart/add/{id}', name:'add_to_cart')]
+    #[Route('/panier', name: 'app_cart')]
+    public function index(Cart $cart): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getFull()
+        ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'add_to_cart')]
     public function add(Cart $cart, $id): Response
     {
         $cart->add($id);
-        
-        return $this->render('cart/index.html.twig');
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove', name: 'remove_my_cart')]
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+        return $this->redirectToRoute('app_products');
+    }
+
+    #[Route('/cart/delete/{id}', name: 'delete_to_cart')]
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/decrease/{id}', name: 'decrease_to_cart')]
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('app_cart');
     }
 }
